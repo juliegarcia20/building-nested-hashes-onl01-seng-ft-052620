55377c78b91968af1843a5e41a0e5c197be68d5a
Julie Garcia <juliegarcia.cali@gmail.com>
2020-06-04 16:04:05 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,15 +1,15 @@
-epic_tragedy = {
-   :montague => {
+def second_challenge
+  epic_tragedy = {
+    :montague => {
       :patriarch => {},
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-   },
-   :capulet => {
+      },
+    :capulet => {
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
-   }
-}
- 
+      }}
+end
